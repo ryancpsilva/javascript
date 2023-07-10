5b1b719f03ba2e9f2625492d6997c7dc7a161b14
ryancpsilva <128498481+ryancpsilva@users.noreply.github.com>
2023-07-10 00:21:31 -0300

    1

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,14 @@
-alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if ( hora >= 0 && hora < 12){
+        //Bom dia
+    }else if (hora >= 12 && hora <18){
+        //Boa tarde
+    }else{
+        //Boa noite
+    }
+}
