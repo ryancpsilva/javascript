076e4af41124336236757d29a490944e687a311b
ryancpsilva <128498481+ryancpsilva@users.noreply.github.com>
2023-07-13 14:14:18 -0300

    1

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+/*var c=0
+while (c<100){
+    c++
+    if(c%2==0){
+        console.log(`${c} é par`)
+    }
+    if(c%2!=0){
+        console.log(`${c} é ímpar`)
+    }
+}*/
+
+var n = 100;
+var arr = Array.from(Array(n).keys());
+for (let i in arr){
+    if (i%2==0){
+        console.log(`${i} é par`)
+    }else{
+        console.log(`${i} é ímpar`)
+    }
+    
+}
