fefe0232eaedfe34a85cdd76a5a89a869998893b
ryancpsilva <128498481+ryancpsilva@users.noreply.github.com>
2023-07-13 08:44:58 -0300

    ex014

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -2,13 +2,22 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var hora =12//data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas. `
     if ( hora >= 0 && hora < 12){
+        document.body.className+='dia'
+        msg.innerHTML += 'Bom dia!'
+        img.src = 'imagens/ftmanha.png'
         //Bom dia
     }else if (hora >= 12 && hora <18){
+        document.body.className+='tarde'
+        msg.innerHTML += 'Boa tarde!'
+        img.src='imagens/fttarde.png'
         //Boa tarde
     }else{
+        document.body.className+='noite'
+        msg.innerHTML += 'Boa noite!'
+        img.src="imagens/ftnoite.png"
         //Boa noite
     }
 }
