943948cb2badacb1a4f8f720df224a7decaf15eb
ryancpsilva <128498481+ryancpsilva@users.noreply.github.com>
2023-07-13 08:53:31 -0300

    Update script.js

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -2,7 +2,7 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
     var data = new Date()
-    var hora =12//data.getHours()
+    var hora =data.getHours()
     msg.innerHTML = `Agora são ${hora} horas. `
     if ( hora >= 0 && hora < 12){
         document.body.className+='dia'
