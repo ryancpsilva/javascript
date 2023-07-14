da552c939223090cbe4db1a4a0d866f26aa40039
ryancpsilva <128498481+ryancpsilva@users.noreply.github.com>
2023-07-14 16:19:21 -0300

    .

--- aula13/node_35b0013853b5c.tmp.js
+++ aula13/node_35b0013853b5c.tmp.js
@@ -1,7 +1,16 @@
-let i = "Gustavo"
+let i = "gustavo"
 let a = ['Ryan','Marcos','Yasmim', 'Gustavo']
 
-function verificar() {
-    return console.log(a)
+function verificar(str, arr) {
+    for (let j in a){
+        if(str == arr[j]) {
+            if (str === arr[j]) {
+                return console.log("Sim")
+            } else {
+                return console.log("Sim, porém há um problema de digitação")
+            }
+        }
+    }
+    return console.log('Não')
 }
-verificar()
+verificar(i,a)
